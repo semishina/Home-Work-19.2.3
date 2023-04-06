9b89a382385708d50095f6db0b0faeff671edb4c
Elena Semishina <123829392+semishina@users.noreply.github.com>
2023-04-07 03:28:29 +0500

    pozitive tests

--- first_test.py
+++ first_test.py
@@ -7,18 +7,9 @@ class TestCalc:
 
     def test_multiply_calculate_positive(self):
         assert self.calc.multiply(self, 2, 2) == 4
-
-    # def test_multiply_calculate_negative(self):
-    #     assert self.calc.multiply(self, 2, 2) == 5
-
+    
     def test_division_calculate_positive(self):
         assert self.calc.division(self, 9, 3) == 3
-
-    # def test_division_calculate_negative(self):
-    #     assert self.calc.division(self, 9, 3) == 0
-
-    # def test_division_calculate_negative_2(self):
-    #     assert self.calc.division(self, 9, 0) == 0
 
     def test_subtraction_calculate_positive(self):
         assert self.calc.subtraction(self, 10, 3) == 7
@@ -29,9 +20,6 @@ class TestCalc:
     def test_subtraction_calculate_positive_3(self):
         assert self.calc.subtraction(self, -10, 3) == -13
 
-    # def test_subtraction_calculate_negative(self):
-    #     assert self.calc.subtraction(self, 10, 3) == 5
-
     def test_adding_calculate_positive(self):
         assert self.calc.adding(self, 3, 3) == 6
 
@@ -40,6 +28,4 @@ class TestCalc:
 
     def test_adding_calculate_positive_3(self):
         assert self.calc.adding(self, -3, -3) == -6
-
-    # def test_adding_calculate_negative(self):
-    #     assert self.calc.adding(self, 3, 3) == 4
+    
